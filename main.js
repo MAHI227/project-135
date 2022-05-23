ebc4da25811c4041c7f0393299ae0d1491e9a91d
Itz_Mahi_Offical <85633027+MAHI227@users.noreply.github.com>
2022-05-23 15:11:04 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,35 @@
+canvas
+{
+    border:2px solid white;
+    border-radius: 10px;
+    margin-top 130;
+    box-shadow: 5px 5px 5px white;
+}
+body
+{
+   background-size: cover;
+}
+.heading
+{
+    font-size: 23px;
+    width: 100%;
+    margin: 0px;
+}
+#start
+{
+    font-size: 18px;
+}
+#status , #number_of_objects , #start
+{
+font-size: 18px;
+}
+.copy_right_footer 
+{
+    position: fixed;
+    color: 9C9C9C;
+    bottom: 0;
+    width: 100;
+    background-color: white;
+    line-height: 3;
+    text-align: center;
+}
